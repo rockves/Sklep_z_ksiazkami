2398ca6a74ed760b70b4e79a83596c58cb35e7d4
Miłosz <m.99@tlen.pl>
2018-11-14 23:25:48 +0100

    Dodany szkielet strony głównej i usunięty folder HTML

---
+++ PHP/Site/MainPage/mainPage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Miłosz Leszko 4C">
+    <title>Księgarnia internetowa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/CSS/mainPage.css" />
+</head>
+<body>
+    <div id="header"><h1>Księgarnia internetowa</h1></div>
+    <section>
+    <div id="leftMenu"></div>
+    <div id="rightWindow"></div>
+    </section>
+    <footer><p>Miłosz Leszko 4C</p></footer>
+</body>
+</html>
