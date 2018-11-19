42a1aad8e7d59f251b400c6124a688bf46a52cf9
Miłosz <m.99@tlen.pl>
2018-11-19 21:11:43 +0100

    Robocza wersja szkieletu strony dla zapytań php

--- PHP/Site/MainPage/mainPage.php
+++ PHP/Site/MainPage/mainPage.php
@@ -9,18 +9,14 @@ require_once(__DIR__.'\Untitles\connection.php');
     <meta name="author" content="Miłosz Leszko 4C">
     <title>Księgarnia internetowa</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="../../../CSS/mainPage.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="" />
 </head>
 
 <body>
-    <div id="header">
-        <span>Księgarnia internetowa</span>
+    <div id="header"><span>Księgarnia internetowa</span></div><br>
+    <div>
+        <?php?>
     </div>
-    <div id="leftMenu">Lewy</div>
-    <div id="rightWindow"></div>
-    <footer>
-        <span>Miłosz Leszko 4C</span>
-    </footer>
 </body>
 
 </html>
